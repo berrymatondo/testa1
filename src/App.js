50ad916a1a0ca0d6d55aaf433fd77a4604b1f759
unknown <berrymatondo@gmail.com>
2022-08-24 09:49:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
-const socket = io.connect("http://localhost:3001");
+//const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://jeteste1.herokuapp.com");
 
 function App() {
   const [message, setMessage] = useState("");
