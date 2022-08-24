ffb4e5697b88702eacf3c1207d22249e2e3c1e04
unknown <berrymatondo@gmail.com>
2022-08-24 08:06:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageRec, setMessageRec] = useState("");
+  const sendHandler = () => {
+    //emit event
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageRec(data.message);
+    });
+  }, [socket]);
+  return (
+    <div className="App">
+      <input onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={sendHandler}>Send</button>
+      <h1>Message:</h1>
+      {messageRec}
+    </div>
+  );
+}
+
+export default App;
